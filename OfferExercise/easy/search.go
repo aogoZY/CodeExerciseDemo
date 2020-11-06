ecbb2929634e3c31f6700bc9bd87872184c60eaa
周阳 <zhouyang@zhouyangdeMacBook-Pro.local>
2020-11-07 01:10:16 +0800

    half search

---
+++ OfferExercise/easy/search.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+//二分查找 有序序列 中间值与目标值比较 不断缩小范围
+func search(nums []int, target int) int {
+	var left, right, mid int
+	right = len(nums) - 1
+	for left <= right {
+		mid = (left + right) / 2
+		if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] == target {
+			return mid
+		}
+	}
+	return -1
+
+}
+
+func main() {
+	input := []int{7,1,5,3,6,4}
+	res := maxProfit(input)
+	fmt.Println(res)
+}
+
+func maxProfit(prices []int)int{
+	var list []int
+	for i:=0;i<len(prices);i++{
+		for j:=i+1;j<len(prices);j++{
+			if prices[j]-prices[i]>0{
+				list=append(list,prices[j]-prices[i])
+			}
+		}
+	}
+	fmt.Println(list)
+	sortedList :=sortList(list)
+	return sortedList[len(list)-1]
+}
+
+func sortList(input []int)[]int{
+	for i:=0;i<len(input);i++{
+		for j:=0;j<len(input)-i-1;j++{
+			if input[j]>input[j+1]{
+				input[j],input[j+1]=input[j+1],input[j]
+			}
+		}
+	}
+	fmt.Println(input)
+	return input
+}
--- jianzhioffer.md
+++ jianzhioffer.md
@@ -834,3 +834,34 @@ func hasPathSum(root *TreeNode, sum int) bool {
    return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
 }
 ```
+
+#### [101. 对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/)
+
+给定一个二叉树，检查它是否是镜像对称的。
+
+![image-20201105002452139](/Users/zhouyang/Library/Application Support/typora-user-images/image-20201105002452139.png)
+
+notice:若当前节点的左子树和右子树是对称的则对称
+
+```
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isSymmetricByLeftAndRight(root.Left,root.Right)
+}
+
+func isSymmetricByLeftAndRight(left *TreeNode,right *TreeNode)bool{
+	if left==nil && right==nil{
+		return true
+	}
+	if left==nil ||right==nil{
+		return false
+	}
+	if left.Val!=right.Val{
+		return false
+	}
+	return isSymmetricByLeftAndRight(left.Left,right.Right)&&isSymmetricByLeftAndRight(left.Right,right.Left)
+}
+```
+
